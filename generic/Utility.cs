94dd7868a4bb82617d0eb79d6ccd786dd35c6d6c
MeravigliosaCreatura <tuttobeneee>
2017-04-20 11:39:09 +0300

    no comment

---
+++ generic/Utility.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public static class Utility
+    {
+        /// <summary>
+        /// Вывод элементов последовательности на экран
+        /// </summary>
+       
+         public static void Print<T>(IEnumerable<T> smth)
+         {
+             
+             foreach (T value in smth)
+             {
+                 
+                 Console.WriteLine(value);
+
+             }
+         }
+        
+        /// <summary>
+         /// Сортировка
+        /// </summary>
+       
+        public static void Sort<T> (IList <T> smth) where T: IComparable <T>
+        {
+           for(int i=0; i<smth.Count-1;i++)
+           {
+               for (int k = 0; k < smth.Count-i-1; k++)
+               {
+                   if (smth[k].CompareTo(smth[k + 1]) > 0)
+                   {
+                       T swap = smth[k];
+                       smth[k] = smth[k + 1];
+                       smth[k + 1] = swap;
+                   }
+               }
+           }
+        }
+    }   
+}
+
