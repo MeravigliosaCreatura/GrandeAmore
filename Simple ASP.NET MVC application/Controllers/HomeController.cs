0290b96a82b5d8b33621e68b20148c4a250a18c6
MeravigliosaCreatura <tuttobeneee>
2017-04-20 12:01:38 +0300

    no message

---
+++ Simple ASP.NET MVC application/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<Footballer> footballerlist;
+        public ActionResult Index()
+        {
+            string json = System.IO.File.ReadAllText(@"C:\Users\ITGigant\documents\visual studio 2013\Projects\WebApplication2\WebApplication2\App_Data\footballer.json", Encoding.Default);
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+            footballerlist = ser.Deserialize<List<Footballer>>(json);
+            return View(footballerlist);
+            
+        }
+     
+    }
+}
---
+++ Simple ASP.NET MVC application/Models/Footballer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Footballer
+    {
+        public long Id { get; set; }
+        [DisplayName("Имя")]
+        public string Name { get; set; }
+        [DisplayName("Фамилия")]
+        public string Surname { get; set; }
+        [DisplayName("Прозвище")]
+        public string Nickname { get; set; }
+        public string Photo { get; set; }
+        [DisplayName("Возраст")]
+        public int Age { get; set; }
+        [DisplayName("Рост")]
+        public int Hight { get; set; }
+        [DisplayName("Вес")]
+        public int Weight { get; set; }
+        [DisplayName("Позиция")]
+        public string Position { get; set; }
+        [DisplayName("Клуб")]
+        public string FootballClub { get; set; }
+        [DisplayName("Номер")]
+        public int Number { get; set; }
+        [DisplayName("Страна")]
+        public string Country { get; set; }
+
+
+    }
+}
